3432cefe5660d81c685e6d50be47e5172d9f6b56
Jalal Agantal <jalal.agantal@g.msuiit.edu.ph>
2014-02-26 08:19:35 -0800

    Update sport_edit.php

--- LeagueManagementSystem/application/views/sport_edit.php
+++ LeagueManagementSystem/application/views/sport_edit.php
@@ -5,7 +5,7 @@
 </style>
 <form action=<?php echo base_url()?>index.php/sportController/update method="post" accept-charset="utf-8">
 <input type="hidden" name="sport_id" value="<?php echo  $row[0]->sport_id;?>">
-<p>sportname<input type="text" name="sportname" value="<?php echo $row[0]->sportname; ?>"></p><input type="submit" name="" value="Edit Sport"></form>
+<p>Sportname: <input type="text" name="sportname" value="<?php echo ucwords($row[0]->sportname); ?>"></p><input type="submit" name="" value="Edit Sport"></form>
 <?php 
 
 echo '<p1>'.validation_errors().'</p1>'; 
