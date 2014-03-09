bb986971412c5178f5a1be72753919c17eb3e0c1
Jalal Agantal <jalal.agantal@g.msuiit.edu.ph>
2014-03-09 09:59:19 -0700

    Update sport.php

--- LeagueManagementSystem/application/models/sport.php
+++ LeagueManagementSystem/application/models/sport.php
@@ -1,47 +1,15 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Sport extends CI_Model
+<?php 
+class Sport
 {
 	private $sportname;
-	function __construct()
-	{
-		parent::__construct();
-	}
-	
-	function constructor($sportname)
+	function __construct($sportname)
 	{
 		$this->sportname=$sportname;
-		return $this;
 	}
 	
 	function getSportname()
 	{
 		return $this->sportname;
-	}
-	
-	function setSportName($sportname)
-	{
-		$this->db->query("UPDATE sport SET sportname='$sportname' WHERE sportname='$this->sportname'");
-	}
-	function sportnameIsBlank()
-	{
-		if(trim($this->sportname)=="")
-			return TRUE;
-		else
-			return FALSE;
-	}
-	function sportnameExist()
-	{
-		$sportname=strtolower($this->sportname);
-		$result=$this->db->query("SELECT * FROM sport where sportname='$sportname' AND accessible='true'");
-		if($result->num_rows()>0)
-			return TRUE;
-		else
-			return FALSE;
-	}
-	
-	function getSportById($id)
-	{
-		return $result=$this->db->query("SELECT * FROM sport where sport_id='$id' AND accessible='true'");
 	}
 }
 ?>
